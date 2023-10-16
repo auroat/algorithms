cc91b0b98be8b39ef59b5c6c005762cf694fee21
Aurel Pintea <insanovation@gmail.com>
2023-10-16 19:19:58 +0100

    Add FizzBuzz - StringBuilder approach.

--- src/main/java/com/practice/algorithms/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/practice/algorithms/fizzbuzz/FizzBuzz.java
@@ -3,20 +3,24 @@ package com.practice.algorithms.fizzbuzz;
 public class FizzBuzz {
 
     public static String[] fizzBuzz(int input) {
-        String[] result = new String[input];
+        String[] outcome = new String[input];
 
         for (int i = 1; i <= input; i++) {
+            StringBuilder result = new StringBuilder();
+
             if (i % 3 == 0) {
-                result[i - 1] = "Fizz";
-            } else if (i % 5 == 0) {
-                result[i - 1] = "Buzz";
-            } else if (i % 3 == 0 && i % 5 == 0) {
-                result[i - 1] = "FizzBuzz";
-            } else
-                result[i - 1] = String.valueOf(i);
+                result.append("Fizz");
+            }
+            if (i % 5 == 0) {
+                result.append("Buzz");
+            }
+            if (result.isEmpty())
+                result.append(i);
+
+            outcome[i - 1] = result.toString();
         }
 
-        return result;
+        return outcome;
 
         // TC = O(n)
         // SC = O(1)
